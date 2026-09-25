f4c929df47883c40a2598ae1fc8388e161c62182
BDuc1906 <duc1962005@gmail.com>
2026-09-25 13:49:23 +0700

    fix

--- src/shared/providers/AuthProvider.jsx
+++ src/shared/providers/AuthProvider.jsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 
 export default function AuthProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Suspense fallback={null}>
+        {children}
+      </Suspense>
+    </SessionProvider>
+  );
 }
